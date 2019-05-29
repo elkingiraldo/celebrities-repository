0785eae3877859525b4d608a88c1b453f64fca8d
elkin.giraldo <elkin.giraldo@CO-IT002585.globant.com>
2019-05-29 14:54:27 -0500

    Add algorithm to entire project and fix code coverage

--- celebrities/src/main/java/com/globant/tests/celebrities/controller/CelebrityController.java
+++ celebrities/src/main/java/com/globant/tests/celebrities/controller/CelebrityController.java
@@ -2,6 +2,7 @@ package com.globant.tests.celebrities.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.globant.tests.celebrities.service.CelebrityService;
@@ -12,7 +13,8 @@ import com.globant.tests.celebrities.service.CelebrityService;
  * @author elkin.giraldo
  *
  */
-@RestController(value = "/celebrity")
+@RestController
+@RequestMapping("/celebrity")
 public class CelebrityController {
 
 	@Autowired
--- celebrities/src/main/resources/import.sql
+++ celebrities/src/main/resources/import.sql
@@ -1,4 +1,8 @@
 /* Populate tables */
-INSERT INTO person (first_name, last_name, is_celebrity) VALUES('Elkin', 'Giraldo', false);
-INSERT INTO person (first_name, last_name, is_celebrity) VALUES('Martha', 'Pinedo', true);
-INSERT INTO person (first_name, last_name, is_celebrity) VALUES('Jairo', 'Jimenez', false);
+INSERT INTO person (first_name, last_name) VALUES('Elkin', 'Giraldo');
+INSERT INTO person (first_name, last_name) VALUES('Martha', 'Pinedo');
+INSERT INTO person (first_name, last_name) VALUES('Jairo', 'Jimenez');
+
+INSERT INTO person_known_people (person_id, known_people) VALUES(1, 3);
+INSERT INTO person_known_people (person_id, known_people) VALUES(1, 2);
+INSERT INTO person_known_people (person_id, known_people) VALUES(3, 2);
