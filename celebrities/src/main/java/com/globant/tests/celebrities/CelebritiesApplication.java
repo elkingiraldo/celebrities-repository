8607eecf356458cefb0666cbf08df7c2612b4405
elkin.giraldo <elkin.giraldo@CO-IT002585.globant.com>
2019-05-28 20:07:27 -0500

    Spring boot base project running

---
+++ celebrities/src/main/java/com/globant/tests/celebrities/CelebritiesApplication.java
@@ -0,0 +1,13 @@
+package com.globant.tests.celebrities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CelebritiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CelebritiesApplication.class, args);
+	}
+
+}
