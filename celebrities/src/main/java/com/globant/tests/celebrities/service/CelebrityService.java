25df648f1e2199ee4684b4e5d8608b79e6140f46
elkin.giraldo <elkin.giraldo@CO-IT002585.globant.com>
2019-05-28 22:35:27 -0500

    Refactor to add utility constants

--- celebrities/src/main/java/com/globant/tests/celebrities/service/CelebrityService.java
+++ celebrities/src/main/java/com/globant/tests/celebrities/service/CelebrityService.java
@@ -5,12 +5,10 @@ import org.springframework.stereotype.Service;
 
 import com.globant.tests.celebrities.model.entity.Person;
 import com.globant.tests.celebrities.model.repository.IPersonRepository;
+import com.globant.tests.celebrities.util.Constants;
 
 @Service
 public class CelebrityService {
-
-	private static final String CELEBRITY_FOUND = "There is a celebrity in the team with the following information => Fist Name: %s, Last Name: %s";
-	private static final String CELEBRITY_NOT_FOUND = "There isn't a celebrity in the team";
 
 	@Autowired
 	private IPersonRepository personRepository;
@@ -20,10 +18,10 @@ public class CelebrityService {
 		final Person celebrity = personRepository.findByIsCelebrity(true);
 
 		if (celebrity != null) {
-			return String.format(CELEBRITY_FOUND, celebrity.getFirstName(), celebrity.getLastName());
+			return String.format(Constants.CELEBRITY_FOUND, celebrity.getFirstName(), celebrity.getLastName());
 		}
 
-		return CELEBRITY_NOT_FOUND;
+		return Constants.CELEBRITY_NOT_FOUND;
 	}
 
 }
---
+++ celebrities/src/main/java/com/globant/tests/celebrities/util/Constants.java
@@ -0,0 +1,8 @@
+package com.globant.tests.celebrities.util;
+
+public final class Constants {
+
+	public static final String CELEBRITY_FOUND = "There is a celebrity in the team with the following information => Fist Name: %s, Last Name: %s";
+	public static final String CELEBRITY_NOT_FOUND = "There isn't a celebrity in the team";
+
+}
